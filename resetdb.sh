3daac0dd5ffd8e14b3e59aa545f2bb0160b09275
Akene Uzezi <xtremejoexj@gmail.com>
2026-06-24 14:58:24 +0100

    update resetdb script

--- resetdb.sh
+++ resetdb.sh
@@ -2,5 +2,11 @@ set -e
 echo "Removing all docker containers and volumes"
 docker rm -f $(docker ps -aq)
 docker volume rm $(docker volume ls -q)
-echo "Starting docker compose"
-docker compose up
+read -p "Do you want to start containers? (y/n) " choice
+if [ "$choice" != "y" ]; then
+    echo "container initialization cancelled"
+    exit 0
+else
+    echo "Starting docker compose"
+    docker compose up -d
+fi
