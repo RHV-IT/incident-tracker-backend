30208a88b1d660fa0c89eb18c9a7d5932922032b
Akene Uzezi <xtremejoexj@gmail.com>
2026-06-25 09:57:55 +0100

    add who was informed section to the struct

--- internal/db/incidentmanagement.go
+++ internal/db/incidentmanagement.go
@@ -8,9 +8,18 @@ type IncidentManagementModel struct {
 
 type IncidentManagement struct {
 	IncidentId int `json:"incidentId" binding:"required"`
+
 	ImpactOnService string `json:"impactOnService" binding:"required"`
 	ContributoryFactors string `json:"contributoryFactors" binding:"required"`
 	ActionsTakenOutcomes string `json:"actionsTakenOutcomes" binding:"required"`
 	Recommendations string `json:"recommendations" binding:"required"`
 	LessonsLearned string `json:"lessonsLearned" binding:"required"`
+
+	InformedPatient bool `json:"informedPatient,omitempty"`
+	InformedRelative bool `json:"informedRelative,omitempty"`
+	InformedSeniorManager bool `json:"informedSeniorManager,omitempty"`
+	InformedPharamacist bool `json:"informedPharmacist,omitempty"`
+	PoliceIncidentNumber string `json:"policeIncidentNumber,omitempty"`
+	InformedOther string `json:"informedOther,omitempty"`
+
 }
