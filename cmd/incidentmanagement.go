35e0b9d3c611d3c68cc4051cf429952cb3e82761
Akene Uzezi <xtremejoexj@gmail.com>
2026-06-25 12:11:30 +0100

    add role check to the route

--- cmd/incidentmanagement.go
+++ cmd/incidentmanagement.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (a *application) submitIncidentManagement(c *gin.Context) {
-
+	userRole := c.GetString("userRole")
+	if userRole != "admin" {
+		c.JSON(http.StatusForbidden, gin.H{"error": "Unauthorized. Must be an admin"})
+		return
+	}
 }
