77a2764a52f9abf5a8c317413ffbe4836c9c4a80
Akene Uzezi <xtremejoexj@gmail.com>
2026-06-25 13:03:17 +0100

    completed insert function

--- internal/db/incidentmanagement.go
+++ internal/db/incidentmanagement.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -11,6 +12,7 @@ type IncidentManagementModel struct {
 }
 
 type IncidentManagement struct {
+	Id int `json:"id"`
 	IncidentId int `json:"incidentId" binding:"required"`
 
 	ImpactOnService string `json:"impactOnService" binding:"required"`
@@ -44,7 +46,24 @@ type IncidentManagement struct {
 }
 
 func(m *IncidentManagementModel) SubmitReport(ctx context.Context, incident *IncidentManagement) (IncidentManagement, error) {
-	var incidentManagement IncidentManagement
-
-	return incidentManagement, nil
+	query := `
+		INSERT INTO incident_management (
+			incident_id, impact_on_service, contributory_factors, actions_taken_outcomes, recommendations, lessons_learned,
+			informed_patient, informed_relative, informed_senior_manager, informed_pharmacist, police_incident_number, informed_other,
+			risk_severity, risk_likelihood, risk_rating,
+			ohs_absence_over_3_days, ohs_act_of_violence_or_danger, ohs_hospitalisation_over_24_hours, ohs_staff_name, ohs_staff_dob, ohs_staff_address,
+			manager_name, manager_signature, manager_designation, manager_date
+		)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28)
+		RETURNING id;
+	`
+	err := m.DB.QueryRow(ctx, query, incident.IncidentId, incident.ImpactOnService, incident.ContributoryFactors, incident.ActionsTakenOutcomes, incident.Recommendations, incident.LessonsLearned,
+		incident.InformedPatient, incident.InformedRelative, incident.InformedSeniorManager, incident.InformedPharmacist, incident.PoliceIncidentNumber, incident.InformedOther,
+		incident.RiskSeverity, incident.RiskLikelihood, incident.RiskRating,
+		incident.OhsAbsenceOver3Days, incident.OhsActOfViolenceOrDanger, incident.OhsHospitalisationOver24Hours, incident.OhsStaffName, incident.OhsStaffDob, incident.OhsStaffAddress,
+		incident.ManagerName, incident.ManagerSignature, incident.ManagerDesignation, incident.ManagerDate).Scan(&incident.Id)
+	if err != nil {
+		return IncidentManagement{}, fmt.Errorf("database query error: %w", err)
+	}
+	return *incident, nil
 }
