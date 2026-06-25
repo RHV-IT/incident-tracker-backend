6d536e448356a7348d9a7cd89b661bf9ac0add65
Akene Uzezi <xtremejoexj@gmail.com>
2026-06-25 09:50:58 +0100

    start writing api struct

--- internal/db/incidentmanagement.go
+++ internal/db/incidentmanagement.go
@@ -6,4 +6,11 @@ type IncidentManagementModel struct {
 	DB *pgxpool.Pool
 }
 
-type IncidentManagement struct {}
+type IncidentManagement struct {
+	IncidentId int `json:"incidentId" binding:"required"`
+	ImpactOnService string `json:"impactOnService" binding:"required"`
+	ContributoryFactors string `json:"contributoryFactors" binding:"required"`
+	ActionsTakenOutcomes string `json:"actionsTakenOutcomes" binding:"required"`
+	Recommendations string `json:"recommendations" binding:"required"`
+	LessonsLearned string `json:"lessonsLearned" binding:"required"`
+}
