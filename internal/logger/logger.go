8621aab7157df16a79bfaa7a96f67e4bfa484b3e
Akene Uzezi <xtremejoexj@gmail.com>
2026-07-04 17:25:52 +0100

    wrote comment logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -3,24 +3,38 @@ package logger
 import (
 	"log"
 	"os"
+	"path/filepath"
 )
 
-var ErrorFileLogger *log.Logger
-var UpdateIncidentLogger *log.Logger
+var (
+	ErrorFileLogger      *log.Logger
+	UpdateIncidentLogger *log.Logger
+	CommentLogger        *log.Logger
+)
 
 func InitLogger() {
-	if err := os.MkdirAll("logs", 0755); err != nil {
+	logDir := "logs"
+	if err := os.MkdirAll(logDir, 0o755); err != nil {
 		log.Fatalf("Failed to create log directory: %v", err)
 	}
-	errFile, err := os.OpenFile("backgroundErrors.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	errFilePath := filepath.Join(logDir, "backgroundErrors.log")
+	errFile, err := os.OpenFile(errFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o666)
 	if err != nil {
 		log.Fatalf("Failed to open error log file: %v", err)
 	}
 	ErrorFileLogger = log.New(errFile, "Async Error: ", log.Ldate|log.Ltime|log.Lshortfile)
 
-	updateFile, err := os.OpenFile("updateIncidents.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	updateFilePath := filepath.Join(logDir, "updateIncidents.log")
+	updateFile, err := os.OpenFile(updateFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o666)
 	if err != nil {
 		log.Fatalf("Failed to open update log file: %v", err)
 	}
 	UpdateIncidentLogger = log.New(updateFile, "Update Incident: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	commentFilePath := filepath.Join(logDir, "comments.log")
+	commentFile, err := os.OpenFile(commentFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o666)
+	if err != nil {
+		log.Fatalf("Failed to open comments log file: %v", err)
+	}
+	CommentLogger = log.New(commentFile, "Comment: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
