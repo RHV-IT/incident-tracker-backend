2c003557d9ffd9aef7358592e519219c01cac57a
Akene Uzezi <xtremejoexj@gmail.com>
2026-07-08 12:28:16 +0100

    remove redundant setupdb function and add custom compiler flag to remove testhelpers.go from prod build

--- internal/db/testhelpers.go
+++ internal/db/testhelpers.go
@@ -1,4 +1,4 @@
-//go:test build
+//go:build test
 
 package db
 
@@ -10,8 +10,6 @@ import (
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
 )
 
@@ -93,60 +91,6 @@ func SetupTestDBSuite() (*pgxpool.Pool, func(), error) {
 	}
 
 	return pool, cleanup, nil
-}
-
-func SetupTestDB(t *testing.T) *pgxpool.Pool {
-	ctx := context.Background()
-	pgContainer, err := postgres.Run(
-		ctx, "postgres:16-alpine",
-		postgres.WithDatabase("testdb"),
-		postgres.WithUsername("test"),
-		postgres.WithPassword("test"),
-	)
-	require.NoError(t, err)
-	t.Cleanup(func() { pgContainer.Terminate(ctx) })
-
-	connStr, _ := pgContainer.ConnectionString(ctx, "sslmode=disable")
-	config, err := pgxpool.ParseConfig(connStr)
-	assert.NoError(t, err)
-	config.MaxConns = 10
-	config.MinConns = 2
-	pool, err := pgxpool.NewWithConfig(ctx, config)
-	require.NoError(t, err)
-
-	var pingErr error
-	for i := 0; i < 10; i++ {
-		pingErr = pool.Ping(ctx)
-		if pingErr == nil {
-			break
-		}
-		time.Sleep(500 * time.Millisecond)
-	}
-	require.NoError(t, pingErr, "Postgres failed to become ready in time")
-
-	dir, err := os.Getwd()
-	require.NoError(t, err)
-
-	for {
-		if _, err := os.Stat(filepath.Join(dir, "go.mod")); err == nil {
-			break
-		}
-		parent := filepath.Dir(dir)
-		if parent == dir {
-			t.Fatal("could not find project root containing go.mod")
-		}
-		dir = parent
-	}
-
-	schemaPath := filepath.Join(dir, "tables.sql")
-
-	schema, err := os.ReadFile(schemaPath)
-	require.NoError(t, err)
-
-	_, err = pool.Exec(ctx, string(schema))
-	require.NoError(t, err)
-
-	return pool
 }
 
 func TruncateTables(t *testing.T, pool *pgxpool.Pool) {
