ce39a85a7a9cf3709c201c7c572564c78d934a24
Akene-Uzezi <xtremejoexj@gmail.com>
2026-07-10 09:01:18 +0100

    add test disable unauthorized

--- cmd/users_test.go
+++ cmd/users_test.go
@@ -68,3 +68,37 @@ func TestUpdateSuccess(t *testing.T) {
 
 	assert.Equal(t, "manager", response["role"])
 }
+
+func TestDisableUnauthorized(t *testing.T) {
+	db.TruncateTables(t, testPool)
+
+	gin.SetMode(gin.TestMode)
+
+	a := &application{
+		origins: "*",
+		models:  db.NewModels(testPool),
+	}
+
+	err := insertUser(a, t)
+	assert.NoError(t, err)
+
+	payload := &DisableRequest{
+		Email: "testuser@example",
+	}
+	jsonBody, _ := json.Marshal(&payload)
+
+	r := gin.Default()
+	r.PUT("/api/v1/disable", mockAuthMiddleware("manager"), a.update)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("PUT", "/api/v1/disable", bytes.NewBuffer(jsonBody))
+
+	r.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusForbidden, w.Code)
+
+	var response map[string]any
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "Unauthorized. Must be a superadmin", response["error"])
+}
